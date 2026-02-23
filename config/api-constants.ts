87e377672f9f6019ebff5c3a497170705b49fb84
unknown <dheeraj.uparkoti.17@gmail.com>
2026-02-24 04:13:56 +0545

    my jobs fixed fetchingand vercel deployed

--- config/api-constants.ts
+++ config/api-constants.ts
@@ -1,11 +1,11 @@
 
 export class ApiConstants {
-  static useProduction = false ; 
+  static useProduction = true ; 
   
   static get baseUrl(): string {
     const url = this.useProduction 
-      ? "https://hrtgwbtnq9.execute-api.ap-south-1.amazonaws.com/api/v1"
-      // ? "https://5pkarxuueg.execute-api.ap-south-1.amazonaws.com/api/v1"
+      // ? "https://hrtgwbtnq9.execute-api.ap-south-1.amazonaws.com/api/v1"
+      ? "https://5pkarxuueg.execute-api.ap-south-1.amazonaws.com/api/v1"
       :"http://192.168.1.75:8000/api/v1";
     
     // console.log(' API URL:', url); 
