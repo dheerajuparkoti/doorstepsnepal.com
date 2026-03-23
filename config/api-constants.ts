c9447ad9076dfeded64ed883c110a4e6d5d517cc
unknown <dheeraj.uparkoti.17@gmail.com>
2026-03-23 17:40:10 +0545

    chaitra 09 gate

--- config/api-constants.ts
+++ config/api-constants.ts
@@ -6,8 +6,9 @@ export class ApiConstants {
   static get baseUrl(): string {
     const url = this.useProduction 
       // ? "https://hrtgwbtnq9.execute-api.ap-south-1.amazonaws.com/api/v1"
-      ? "https://5pkarxuueg.execute-api.ap-south-1.amazonaws.com/api/v1"
-      :"http://192.168.1.64:8000/api/v1";
+      // ? "https://5pkarxuueg.execute-api.ap-south-1.amazonaws.com/api/v1"
+      ?"https://hrtgwbtnq9.execute-api.ap-south-1.amazonaws.com/api/v1"
+      :"http://192.168.1.74:8000/api/v1";
     
     // console.log(' API URL:', url); 
     return url;
